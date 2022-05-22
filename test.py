f3b6987839ec1e3dfda3418b33a6386de968552f
Eli Weber <eliweber2001@gmail.com>
2022-05-22 09:01:10 -0400

    Changed

--- test.py
+++ test.py
@@ -3,23 +3,42 @@ import openpyxl
 
 
 workbook = openpyxl.load_workbook("tester.xlsx")
+formattedWorkbook = openpyxl.load_workbook("formatted.xlsx")
 sheet = workbook.active
+max = sheet.max_row
+newSheet = formattedWorkbook.active
+arrayOfObjects = []
 dictionary = {}
 
 
 
-val=sheet.cell(row=1, column=1)
-obj = val.value
-stripper = obj.replace('"', '')
-individuals = stripper.split(",")
+for i in range(1, max + 1):
+    val=sheet.cell(row=i, column=1)
+    obj = val.value
+    stripper = obj.replace('"', '')
+    individuals = stripper.split(",")
+    newObj = {}
+    for temp in individuals:
+        finalSplit = temp.split(':')
+        key = finalSplit[0]
+        val = finalSplit[1]
+        newObj[key] = val
+        if dictionary.get(key):
+            dictionary[key]=dictionary[key]+1
+        else:
+            dictionary[key]=1
+    arrayOfObjects.append(newObj)
 
 
 
-for temp in individuals:
-    finalSplit = temp.split(':')
-    key = finalSplit[0]
-    val = finalSplit[1]
-    dictionary[key]=val
+loopCount = 1
+for key, val in dictionary.items():
+    cell=newSheet.cell(row=1, column=loopCount)
+    cell.value = key
+    loopCount += 1
 
+print(arrayOfObjects)
 
-print(dictionary["appname"])
+workbook.save("tester.xlsx")
+formattedWorkbook.save("formatted.xlsx")
+#formattedWorkbook.save()
